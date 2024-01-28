82093cfd0a663e50d6688321644b859540d2a155
Marwin <80853859+Marwin-Rdgs@users.noreply.github.com>
2024-01-28 18:32:32 +0100

    feat: add button and grid

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Home</h1>
+    <main class="main">
+        <button>Likes</button>
+        <Card />
+        <button>Dislikes</button>
+    </main>
 </template>
+
+
+<style lang="scss">
+    main {
+        display: grid;
+        grid-template-columns: 30% 30% 30%;
+        justify-items: center;
+
+        button {
+            border-radius: 100%;
+            background-color: $d-pink;
+
+        }
+    }
+</style>
